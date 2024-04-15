09b375d64e1288b6368992771deec0669db98ca3
EISENHOWER <chepkosum@gmail.com>
2024-04-15 22:57:57 +0300

    Slider - Edit Update with Image, FormRequest & Delete data with Image

---
+++ resources/views/admin/slider/edit.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.admin')
+
+@section('content')
+
+    <div class="row">
+        <div class="col-md-12 grid-margin">
+
+            @if (session('message'))
+
+            <div class="alert alert-success">{{session('message')}}</div>
+
+            @endif
+            <div class="card">
+                <div class="card-header">
+                    <h3>Update Slider
+                        <a href="{{ url('admin/sliders') }}" class="btn btn-primary text-white  btn-sm  float-end  ">Back</a>
+                    </h3>
+                </div>
+                <div class="card-body">
+                    <form action="{{url('admin/sliders/'.$slider->id)}}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
+
+                        <div class="mb-3">
+                            <label >Title</label>
+                            <input type="text" name="title" value="{{$slider->title}}" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label >Description</label>
+                            <textarea name="description"  rows="4" class="form-control">{{$slider->title}}</textarea>
+                        </div>
+                        <div class="mb-3">
+                            <label >Image</label>
+                            <input type="file" name="image" class="form-control">
+                            <img src="{{asset("$slider->image")}}" style="width:40px; height:40px;" alt="Slider">
+                        </div>
+                        <div class="mb-3">
+                            <label >Status</label> <br>
+                            <input type="checkbox" {{$slider->status =='1'?'checked':''}} name="status"/> Checked=Hidden, Unchecked=Visible
+                        </div>
+
+                        <div class="mb-3">
+                            <button type="submit" class="btn btn-primary float-end">Update</button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @endsection
