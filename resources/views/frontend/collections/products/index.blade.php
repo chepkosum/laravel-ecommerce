3b32768f10a3ca3703dca7d8f437f126ed10aabf
EISENHOWER <chepkosum@gmail.com>
2024-04-16 22:24:20 +0300

    Converting display products in Livewire Component

--- resources/views/frontend/collections/products/index.blade.php
+++ resources/views/frontend/collections/products/index.blade.php
@@ -20,51 +20,11 @@
             <div class="col-md-12">
                 <h4 class="mb-4">Our Products</h4>
             </div>
-            @forelse ($products as $productItem)
-            <div class="col-md-3">
-                <div class="product-card">
-                    <div class="product-card-img">
-                        @if ($productItem->quantity > 0)
-                            <label class="stock bg-success">In Stock</label>
-                        @else
-                        <label class="stock bg-danger">Out of Stock</label>
-                        @endif
 
-                        @if ($productItem->productImages->count()>0)
-                        <a href="{{url('/collection/'.$productItem->category->slug.'/'.$productItem->slug)}}">
-                            <img src="{{asset($productItem->productImages[0]->image)}}" alt="{{$productItem->name}}">
-                        </a>
-                        @endif
-                    </div>
-                    <div class="product-card-body">
-                        <p class="product-brand">{{$productItem->brand}}</p>
-                        <h5 class="product-name">
-                           <a href="{{url('/collection/'.$productItem->category->slug.'/'.$productItem->slug)}}">
-                            {{$productItem->name}}
-                           </a>
-                        </h5>
-                        <div>
-                            <span class="selling-price">Ksh {{$productItem->selling_price}}</span>
-                            <span class="original-price">Ksh {{$productItem->original_price}}</span>
-                        </div>
-                        {{-- <div class="mt-2">
-                            <a href="" class="btn btn1">Add To Cart</a>
-                            <a href="" class="btn btn1"> <i class="fa fa-heart"></i> </a>
-                            <a href="" class="btn btn1"> View </a>
-                        </div> --}}
-                    </div>
-                </div>
-            </div>
-            @empty
-                <div class="col-md-12">
-                    <h5>No Items Found For {{$category->name}} Category</h5>
-                </div>
-            @endforelse
+            <livewire:frontend.product.index :products="$products" :category="$category" />
 
         </div>
     </div>
 </div>
-
-
 
 @endsection
