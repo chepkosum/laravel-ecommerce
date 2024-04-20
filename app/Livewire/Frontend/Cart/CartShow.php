6c13dd8af401e4e71ebd76edcc65a7f1048d9a93
EISENHOWER <chepkosum@gmail.com>
2024-04-21 01:00:09 +0300

    art item Quantity increment decrement

--- app/Livewire/Frontend/Cart/CartShow.php
+++ app/Livewire/Frontend/Cart/CartShow.php
@@ -9,6 +9,112 @@ class CartShow extends Component
 {
     public $cart;
 
+    public function decrementQuantity(int $cartId){
+
+        $cartData = Cart::where('id', $cartId)->where('user_id', auth()->user()->id)->first();
+        if($cartData){
+
+            if($cartData->productColor()->where('id', $cartData->product_color_id)->exists()){
+
+                $productColor = $cartData->productColor()->where('id', $cartData->product_color_id)->first();
+                if($productColor->quantity > $cartData->quantity){
+
+                    $cartData->decrement('quantity');
+                    $this->dispatch('message',
+                    text: 'Quantity Updated',
+                    type: 'success',
+                    status: 200
+                );
+                }else{
+                    $this->dispatch('message',
+                    text: 'Only '.$productColor->quantity.' Quantity Available',
+                    type: 'success',
+                    status: 200
+                );
+                }
+            }
+            else{
+
+                if($cartData->product->quantity >  $cartData->quantity){
+
+                    $cartData->decrement('quantity');
+                    $this->dispatch('message',
+                    text: 'Quantity Updated',
+                    type: 'success',
+                    status: 200
+
+                );
+            }else{
+                $this->dispatch('message',
+                text: 'Only '.$cartData->product->quantity.' Quantity Available',
+                type: 'success',
+                status: 200
+            );
+            }
+
+            }
+
+        } else{
+            $this->dispatch('message',
+            text: 'Something Went Wrong',
+            type: 'error',
+            status: 404
+        );
+        }
+    }
+
+    public function incrementQuantity(int $cartId){
+
+        $cartData = Cart::where('id', $cartId)->where('user_id', auth()->user()->id)->first();
+        if($cartData){
+
+            if($cartData->productColor()->where('id', $cartData->product_color_id)->exists()){
+
+                $productColor = $cartData->productColor()->where('id', $cartData->product_color_id)->first();
+                if($productColor->quantity > $cartData->quantity){
+                    $cartData->increment('quantity');
+                    $this->dispatch('message',
+                    text: 'Quantity Updated',
+                    type: 'success',
+                    status: 200
+                );
+                }else{
+                    $this->dispatch('message',
+                    text: 'Only '.$productColor->quantity.' Quantity Available',
+                    type: 'success',
+                    status: 200
+                );
+                }
+            }
+            else{
+
+                if($cartData->product->quantity >  $cartData->quantity){
+
+                    $cartData->increment('quantity');
+                    $this->dispatch('message',
+                    text: 'Quantity Updated',
+                    type: 'success',
+                    status: 200
+
+                );
+            }else{
+                $this->dispatch('message',
+                text: 'Only '.$cartData->product->quantity.' Quantity Available',
+                type: 'success',
+                status: 200
+            );
+            }
+
+            }
+
+        } else{
+            $this->dispatch('message',
+            text: 'Something Went Wrong',
+            type: 'error',
+            status: 404
+        );
+        }
+    }
     public function render()
     {
         $this->cart = Cart::where('user_id', auth()->user()->id)->get();
