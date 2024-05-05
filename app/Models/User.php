0dc5f10147612cb6dfe0af5aa397c6ff2dc81d3c
EISENHOWER <chepkosum@gmail.com>
2024-05-05 20:39:44 +0300

    Create User profile with hasOne relationship and User Profile Page

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\UserDetail;
 
 class User extends Authenticatable
 {
@@ -22,6 +23,10 @@ class User extends Authenticatable
         'password',
         'role_as',
     ];
+
+    public function userDetail(){
+        return $this->hasOne(UserDetail::class, 'user_id', 'id');
+    }
 
     /**
      * The attributes that should be hidden for serialization.
@@ -45,4 +50,5 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
 }
